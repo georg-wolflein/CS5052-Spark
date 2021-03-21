1e281033fda7f5856d7d4f7a576cc3ef491b035d
Georg Wölflein <georgw7777@gmail.com>
2021-03-21 21:15:30 +0000

    Implement API endpoints for all basic queries #2

--- api/app/data.py
+++ api/app/data.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import pandas as pd
 from pyspark.sql import SparkSession, DataFrame
-import pyspark.sql.functions as sf
+import pyspark.sql.functions as F
 import os
 from pyspark.sql.types import StringType, IntegerType, BooleanType, FloatType, TimestampType
 
@@ -30,7 +30,7 @@ def set_dtypes(df, types: dict):
             bool: BooleanType(),
             "time": TimestampType()
         }[dtype]
-        df = df.withColumn(column, sf.col(column).cast(dtype))
+        df = df.withColumn(column, F.col(column).cast(dtype))
     return df
 
 
@@ -57,11 +57,20 @@ df_tags = read_df("tags.csv", {
     "timestamp": "time"
 })
 
-df_movies = df_movies.withColumn("year",
-                                 sf.substring(df_movies.title, -5, 4).cast(IntegerType()))
+# Remove whitespaces at beginning/end of titles
 df_movies = df_movies.withColumn("title",
-                                 sf.expr("substring(title, 0, length(title)-7)"))
-df_movies = df_movies.withColumn("genres", sf.split("genres", "\|"))
+                                 F.trim("title"))
+# Extract year from title
+df_movies = df_movies.withColumn("year",
+                                 F.regexp_extract("title", r"\((\d{4})\)$", 1)
+                                 .cast(IntegerType()))
+# Remove year from title (if extracted)
+df_movies = df_movies.withColumn("title",
+                                 F.when(F.isnan(F.col("year")), F.col("title"))
+                                 .otherwise(F.expr("substring(title, 0, length(title)-7)")))
+# Explode genres
+df_movies = df_movies.withColumn("genres",
+                                 F.split("genres", "\|"))
 
 
 def search_user(user_id: int) -> DataFrame:
@@ -72,9 +81,14 @@ def search_user(user_id: int) -> DataFrame:
         df_tags.userId == user_id).select("movieId").distinct()
     movies = rated_movies.union(tagged_movies).distinct().join(
         df_movies, on=["movieId"], how="inner")
-    movies = movies.select(movies.movieId, sf.explode(
-        sf.split(movies.genres, "\|")).alias("genre"))
+    movies = movies.select(movies.movieId, F.explode(
+        F.split(movies.genres, "\|")).alias("genre"))
     movies = movies.groupBy("genre").count()
+    return movies
+
+
+def search_movies_by_title(title: str) -> DataFrame:
+    movies = df_movies.filter(f"LOWER(title) LIKE '%{title.lower()}%'")
     return movies
 
 
@@ -91,3 +105,54 @@ def search_movies_by_users(user_ids: [int]) -> DataFrame:
         .filter(f"count = {len(user_ids)}")\
         .drop("count")
     return movies
+
+
+def get_number_of_views_for_movie(movie_id: int) -> int:
+    """Search movie by id, show the number of users that have watched the movie."""
+    rated_movies = df_ratings.where(f"movieId = {movie_id}")
+    tagged_movies = df_tags.where(f"movieId = {movie_id}")
+    movies = rated_movies.join(tagged_movies, on=["userId"], how="outer")
+    movies = movies.select("userId").distinct()
+    return movies.count()
+
+
+def get_rating_for_movie(movie_id: int) -> float:
+    """Search movie by id, show the average rating."""
+    rated_movies = df_ratings.where(
+        f"movieId = {movie_id}").agg({"rating": "avg"})
+    return rated_movies.first()[0]
+
+
+def search_movies_by_genres(genres: [str]) -> DataFrame:
+    """Given a list of genres, search all movies belonging each genre."""
+    n = len(genres)
+    genres = F.array([F.lit(x) for x in genres])
+    return df_movies.filter(
+        F.size(F.array_intersect(F.col("genres"), genres)) == n)
+
+
+def search_movies_by_year(year: int) -> DataFrame:
+    """Search movies by year."""
+    return df_movies.filter(F.col("year") == year)
+
+
+def top_n_movies_by_rating(n: int) -> DataFrame:
+    """List the top N movies with highest rating, ordered by the rating."""
+    return df_ratings.groupBy("movieId")\
+        .agg(F.avg("rating").alias("rating"), F.count("movieId").alias("count"))\
+        .join(df_movies, on=["movieId"], how="inner")\
+        .sort(F.col("rating").desc(), F.col("count").desc())\
+        .limit(n)
+    return top_n
+
+
+def top_n_movies_by_watch_count(n: int):
+    """List the top N movies with the highest number of watches, ordered by the number of watches.
+
+    NOTE: watch count for one movie is total number of people who rated or tagged the movie at least once
+    """
+    return df_ratings.join(df_tags, on=["userId", "movieId"], how="outer")\
+        .select("userId", "movieId").distinct()\
+        .groupBy("movieId").count().join(df_movies, on=["movieId"])\
+        .sort(F.col("count").desc())\
+        .limit(n)
--- api/app/main.py
+++ api/app/main.py
@@ -22,8 +22,16 @@ class Version(BaseModel):
 class Movie(BaseModel):
     movieId: int
     title: str
-    year: int
+    year: typing.Optional[int]
     genres: typing.List[str]
+
+
+class WatchedMovie(Movie):
+    count: int
+
+
+class RatedMovie(WatchedMovie):
+    rating: float
 
 
 def to_base_model_type(cls, row: Row):
@@ -39,6 +47,7 @@ def get_version():
 
 
 @router.get("/users/{user_id}/genres",
+            tags=["users"],
             description="Given a user, get the number of movies watched per genre.",
             response_model=typing.Dict[str, int])
 @dictify
@@ -46,13 +55,80 @@ def search_user(user_id: int):
     return data.search_user(user_id).collect()
 
 
-@router.post("/users/movies/watched",
+@router.get("/movies/search/title",
+            tags=["movies"],
+            description="Search for a movie by title",
+            response_model=typing.List[Movie])
+@listify
+@mapify(partial(to_base_model_type, Movie))
+def search_movies_by_title(title: str):
+    return data.search_movies_by_title(title).collect()
+
+
+@router.get("/movies/search/year",
+            tags=["movies"],
+            description="Search for a movie by year",
+            response_model=typing.List[Movie])
+@listify
+@mapify(partial(to_base_model_type, Movie))
+def search_movies_by_year(year: int):
+    return data.search_movies_by_year(year).collect()
+
+
+@router.post("/movies/search/users",
+             tags=["movies"],
              description="Given a list of users, search all movies watched by each user.",
              response_model=typing.List[Movie])
 @listify
 @mapify(partial(to_base_model_type, Movie))
 def search_movies_by_users(user_ids: typing.List[int]):
     return data.search_movies_by_users(user_ids).collect()
+
+
+@router.post("/movies/search/genres",
+             tags=["movies"],
+             description="Given a list of genres, search all movies belonging each genre.",
+             response_model=typing.List[Movie])
+@listify
+@mapify(partial(to_base_model_type, Movie))
+def search_movies_by_genres(genres: typing.List[str]):
+    return data.search_movies_by_genres(genres).collect()
+
+
+@router.get("/movies/{movie_id}/watched",
+            tags=["movies"],
+            description="Get the number of users that have watched the movie.",
+            response_model=int)
+def get_number_of_views_for_movie(movie_id: int):
+    return data.get_number_of_views_for_movie(movie_id)
+
+
+@router.get("/movies/{movie_id}/rating",
+            tags=["movies"],
+            description="Get the average rating of the movie.",
+            response_model=float)
+def get_rating_for_movie(movie_id: int):
+    return data.get_rating_for_movie(movie_id)
+
+
+@router.get("/movies/top/rated/{n}",
+            tags=["movies"],
+            description="List the top N movies with highest rating, ordered by the rating.",
+            response_model=typing.List[RatedMovie])
+@listify
+@mapify(partial(to_base_model_type, RatedMovie))
+def top_n_movies_by_rating(n: int):
+    return data.top_n_movies_by_rating(n).collect()
+
+
+@router.get("/movies/top/watched/{n}",
+            tags=["movies"],
+            description="List the top N movies with the highest number of watches, ordered by the number of watches.",
+            response_model=typing.List[WatchedMovie])
+@listify
+@mapify(partial(to_base_model_type, WatchedMovie))
+def top_n_movies_by_watch_count(n: int):
+    return data.top_n_movies_by_watch_count(n).collect()
 
 
 app.include_router(router)

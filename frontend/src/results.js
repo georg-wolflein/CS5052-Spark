e571c2160df703c7994cc53051725a92c27a4c89
Georg Wölflein <georgw7777@gmail.com>
2021-03-25 16:50:46 +0000

    Basic API connection #3

--- frontend/src/results.js
+++ frontend/src/results.js
@@ -1,53 +1,55 @@
-import React from 'react';
-import { Redirect } from 'react-router';
-import { validate } from './validate.js'
+import React from "react";
+import { Redirect } from "react-router";
+import { validate } from "./validate.js";
+import { API } from "./api.js";
 
 class Results extends React.Component {
-    _mounted = false;
-    constructor(props) {
-        super(props);
-        this.state = {
-            search: this.props.match.params.search,
-            type: this.props.match.params.type
-        };
+  _mounted = false;
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: this.props.match.params.search,
+      type: this.props.match.params.type,
+    };
+  }
+
+  componentDidMount() {
+    // TODO: Fetch results here from the backend
+    // TODO: Currently simulating async using setTimeout
+    this._mounted = true;
+
+    API.searchMoviesByGenres(["Action", "Crime"]).then(console.log);
+
+    setTimeout(() => {
+      if (this._mounted) {
+        console.log("hi");
+        this.setState({ test: "hi" });
+      }
+    }, 3000);
+  }
+
+  componentWillUnmount() {
+    this._mounted = false;
+  }
+
+  render() {
+    // First get if the state is valid
+    if (!validate("search", this.state) || !validate("type", this.state)) {
+      return <Redirect to="/" />;
     }
 
-    componentDidMount() {
-        // TODO: Fetch results here from the backend
-        // TODO: Currently simulating async using setTimeout
-        this._mounted = true;
-        setTimeout(() => {
-            if(this._mounted) {
-                console.log("hi");
-                this.setState({ test: "hi" });
-            }
-        }, 3000);
+    // Wait for the backend to respond
+    if (this.state.test === undefined) {
+      return <h1>Loading from server...</h1>;
     }
 
-    componentWillUnmount() {
-        this._mounted = false;
-    }
-
-    render() {
-        // First get if the state is valid
-        if(!validate("search", this.state) || !validate("type", this.state)) {
-            return (
-                <Redirect to="/" />
-            );
-        } 
-
-        // Wait for the backend to respond
-        if(this.state.test === undefined) {
-            return (
-                <h1>Loading from server...</h1>
-            );
-        }
-        
-        // Valid state, render the results
-        return (
-            <p>Type: {this.state.type} <br/> search: {this.state.search}</p>
-        );
-    }
+    // Valid state, render the results
+    return (
+      <p>
+        Type: {this.state.type} <br /> search: {this.state.search}
+      </p>
+    );
+  }
 }
 
 export default Results;

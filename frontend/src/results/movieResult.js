082cc29c99d68fe9f6c9e8c54ce24585b7797812
Paul Graham <p@ul.gr>
2021-03-28 16:40:42 +0100

    display movies

--- frontend/src/results/movieResult.js
+++ frontend/src/results/movieResult.js
@@ -1,26 +1,93 @@
 import { API } from './api.js'
 import { BaseResult } from './baseResult.js';
+import { ResultsTable } from './resultsTable.js';
 
 /**
  * Movies class which handles movie API searches
  */
 class MovieResult extends BaseResult {
     async callAPI(search) {
+        this.title = "";
+        this.movies = [];
+        this.heading = [ "Title", "Year", "Genres", "Average Rating", "Watched" ];
+
+        const query = search.toLowerCase();
         return new Promise((resolve, reject) => {
-            API.searchMoviesByTitle(search).then((value) => {
-                this.movies = [];
-                for(const movie of value) {
-                    this.movies.push(movie.title);
-                } resolve();
-            }).catch((reason) => {
-                reject(reason);
-            });
+            // If the query is in the form "top rated [number]", then return the top n
+            if(/^top rated \d+$/.test(query)) {
+                var number = query.substring("top rated ".length);
+                this.title = "Top rated " + number + " movies"; 
+                API.topRatedMovies(number).then((value) => {
+                    this.pushMovies(value, resolve);
+                }).catch((reason) => {
+                    reject(reason);
+                });
+            }
+
+            // If it's in the form "top watched [number]", return that
+            else if(/^top watched \d+$/.test(query)) {
+                var number = query.substring("top watched ".length);
+                this.title = "Top watched " + number + " movies"; 
+                API.topWatchedMovies(number).then((value) => {
+                    this.pushMovies(value, resolve);
+                }).catch((reason) => {
+                    reject(reason);
+                });
+            }
+
+            // Otherwise, see if the movie title is in the form of a year
+            else if(/^\d\d\d\d$/.test(query)) {
+                this.title = "Movies in " + query; 
+                API.searchMoviesByYear(query).then((value) => {
+                    this.pushMovies(value, resolve);
+                }).catch((reason) => {
+                    reject(reason);
+                });
+            }
+
+            // Finally, just search by title
+            else {
+                this.title = "Movies with a title like \"" + search + "\""; 
+                API.searchMoviesByTitle(query).then((value) => {
+                    this.pushMovies(value, resolve);
+                }).catch((reason) => {
+                    reject(reason);
+                });
+            }
         });
     }
     
+    pushMovies(movies, cb) {
+        for(const rawMovie of movies) {
+            var movie = [];
+
+            // The title & year
+            movie.push(rawMovie.title);
+            movie.push(rawMovie.year);
+
+            // The genres
+            var genres = "";
+            for(const genre of rawMovie.genres) {
+                genres += genre + ", ";
+            } movie.push(genres.substring(0, genres.length - 2));
+
+            // Rating
+            movie.push("tbd");
+
+            // Watched
+            movie.push("tbd");
+
+            // Finally push the list to be rendered
+            this.movies.push(movie);
+        } cb();
+    }
+
     draw() {
         return (
-            <h1>Okay {this.movies}</h1>
+            <div id="movies">
+                <h1>{ this.title }</h1><br/>
+                <ResultsTable heading={ this.heading } data={ this.movies } />
+            </div>
         );
     }
 }
---
+++ frontend/src/results/resultsTable.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+export class ResultsTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            heading: this.props.heading,
+            data: this.props.data
+        };
+    }
+
+    renderRow(data, rownum, heading=false) {
+        var render = [];
+        for(var i = 0; i < data.length; i++) {
+            if(heading) render.push(<th key={ "heading_" + i }>{ data[i] }</th>);
+            else render.push(<td key={ "row_" + rownum + "_" + i }>{ data[i] }</td>);
+        } return render;
+    }
+
+    renderData(data) {
+        var rows = [];
+        for(var i = 0; i < data.length; i++) {
+            rows.push(<tr key={ "row_" + i }>{ this.renderRow(data[i], i) }</tr>);
+        } return rows;
+    }
+
+    render() {
+        return (
+            <Table responsive>
+                <thead>
+                    <tr key="heading">
+                        { this.renderRow(this.state.heading, 0, true) }
+                    </tr>
+                </thead>
+                <tbody>
+                    { this.renderData(this.state.data) }
+                </tbody>
+            </Table>
+        );
+    }
+}
+

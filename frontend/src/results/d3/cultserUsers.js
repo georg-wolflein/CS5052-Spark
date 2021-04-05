849a6a162cebbf747a788746c10b1d83a313450b
Paul Graham <p@ul.gr>
2021-04-05 16:50:06 +0100

    some work on the culstering

--- frontend/src/results/d3/cultserUsers.js
+++ frontend/src/results/d3/cultserUsers.js
@@ -14,8 +14,12 @@ class ClusterUsers extends D3Base {
     callAPI() {
         return new Promise((resolve, reject) => {
             API.getGraphOfMutualMovieViews(this.state.query).then((graph) => {
-                console.log(graph);
-                this.setState({ graph: graph });
+                var data = { edges: [], nodes: [] };
+                for(var i = 0; i < graph.nodes['length']; i++) {
+                    data.nodes.push({ value: graph.nodes[i] });
+                } for(i = 0; i < graph.edges["length"]; i++) {
+                    data.edges.push({ source: graph.edges[i]["to"], target: graph.edges[i]["from"], weight: graph.edges[i]["weight"] })
+                } this.setState({ graph: data });
                 resolve();
             }).catch(() => { reject(); });
         });
@@ -23,11 +27,62 @@ class ClusterUsers extends D3Base {
 
     /**
      * Make a force-directed graph of users
+     * @see https://observablehq.com/@d3/force-directed-graph
      */
      renderGraph() {
         this.preRenderGraph();
 
+        const chargeForce = d3.forceManyBody(this.state.graph.edges);
+        chargeForce.strength(d => -100 * d.value);
+        chargeForce.distanceMin(d => d.value);
+
+        const sim = d3.forceSimulation(this.state.graph.nodes)
+            .force("link", d3.forceLink(this.state.graph.edges).id(d => d.value))
+            .force("charge", chargeForce)
+            .force("center", d3.forceCenter(this.width / 2, this.height / 2));
+
         
+        var drag = (sim) => d3.drag()
+                .on("start", (e) => {
+                    if(!e.active) sim.alphaTarget(0.3).restart();
+                    e.subject.fx = e.subject.x;
+                    e.subject.fy = e.subject.y;
+                }).on("drag", (e) => {
+                    e.subject.fx = e.x;
+                    e.subject.fy = e.y;
+                }).on("end", (e) => {
+                    if(!e.active) sim.alphaTarget(0);
+                    e.subject.fx = null;
+                    e.subject.fy = null;
+                });
+
+        const edge = this.svg.append("g")
+            .attr("stroke", "#999")
+            .attr("stroke-opactiy", 0.2)
+            .selectAll("line")
+            .data(this.state.graph.edges).join("line")
+            .attr("stroke-width", d => Math.sqrt(d.weight));
+
+        const node = this.svg.append("g")
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 1.5)
+            .selectAll("circle")
+            .data(this.state.graph.nodes).join("circle")
+            .attr("r", 5)
+            .attr("fill", "#ee3333")
+            .call(drag(sim));
+            
+        node.append("title")
+            .text(d => d.value);
+
+        sim.on("tick", () => {
+            edge.attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+            node.attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
     }
 }
 
